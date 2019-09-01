c573430cd1f1f4fadf2426339a63918831289c3e
Travis CI <travis@travis-ci.org>
2019-09-02 00:46:48 +0300

    Installed Socket.io and deleted ussles stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const router = require('./routes/route');
 
 const app = express();
 
@@ -9,7 +8,6 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const port = process.env.port || 3000;
 
 app.use(express.static(publicDirectoryPath));
-app.use(router);
 
 app.listen(port, (req, res) => {
     console.log('Server is running on port ' + port);
