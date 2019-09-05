ffe51807a0fea4bb53ee5620bb7a4dcaf7ffb9bd
Travis CI <travis@travis-ci.org>
2019-09-05 12:14:35 +0300

    Added WebSockets.io to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, "../public");
-
 const port = process.env.port || 3000;
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, (req, res) => {
-    console.log('Server is running on port ' + port);
+var count = 0;
+io.on('connection', (socket) => {
+    console.log("New Websocket connection...");
+
+    socket.emit('countUpdated', count);
+    socket.on("increment", () => {
+        count++;
+        //socket.emit('countUpdated', count);
+        io.emit('countUpdated', count);
+    })
+});
+
+server.listen(port, () => {
+    console.log('Server is running on port ' + port + "!");
 });
